c464783703b264d7edcff930a5f564717c055ccb
Colin Davey <himself@colindavey.com>
2021-02-27 18:53:56 -0800

    Deleting archives older than X

--- wp_bak/app/wp_bak.py
+++ wp_bak/app/wp_bak.py
@@ -9,7 +9,9 @@
 # docker run -v `pwd`/src:/home/mvp/app/src -v `pwd`/dst:/home/mvp/app/dst -t colindavey/wp_bak:stub
 
 from datetime import datetime
+from fnmatch import fnmatch
 import logging
+import os
 import sys
 import tarfile
 import time
@@ -17,28 +19,44 @@ import time
 SRC_DIR='src/'
 DST_DIR='dst/'
 
-MINUTE=60
-HOUR=60*MINUTE
-DAY=24*HOUR
-WEEK=7*DAY
+MINUTE = 60
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
+WEEK = 7 * DAY
+
+DATE_TIME_FORMAT = "%Y-%m-%d-%H-%M-%S"
 
 # Number of seconds between backups
-PERIOD = 2
-PERIOD_TO_KEEP = 4
+PERIOD = 1
+PERIOD_TO_KEEP = 10
 
 i = 0
-num_reps = 3
+num_reps = 30
 
 logging.basicConfig(level=logging.INFO, stream=sys.stderr)
 log = logging.getLogger(__name__)
 
 while True:
     time.sleep(PERIOD)
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    tar = tarfile.open(DST_DIR+'archive'+timestamp+'tar.gz', mode='w:gz')
+    now = datetime.now()
+    timestamp = now.strftime(DATE_TIME_FORMAT)
+
+    # Archive the directory
+    log.info('Archiving %s', timestamp)
+    tar = tarfile.open(DST_DIR+'archive'+timestamp+'.tar.gz', mode='w:gz')
     tar.add(SRC_DIR)
     tar.close
-    log.info('Archiving %s', timestamp)
+
+    archives = list(filter(lambda x: fnmatch(x, 'archive*.tar.gz'), os.listdir(DST_DIR)))
+    for archive in archives:
+        # Get string representation of archive date
+        archive_date = archive.replace('archive', '').replace('.tar.gz', '')
+        print(datetime.strptime(archive_date, DATE_TIME_FORMAT))
+        # Get age from archive-date string
+        archive_age = now - datetime.strptime(archive_date, DATE_TIME_FORMAT)
+        archive_age = (archive_age.days * DAY) + archive_age.seconds
+        if archive_age > PERIOD_TO_KEEP:
+            os.remove(DST_DIR + archive)
 
     i = i + 1
     if(i > num_reps):
