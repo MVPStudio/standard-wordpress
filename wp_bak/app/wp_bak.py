3a859abc4d7e8e0dd0f4fdedfac2a5f203dee3da
Colin Davey <himself@colindavey.com>
2021-02-27 17:45:44 -0800

    Added logging

--- wp_bak/app/wp_bak.py
+++ wp_bak/app/wp_bak.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 # Todo:
 # -- use timestamp in name
+# -- add logging
 # -- delete older than X
 # -- logic for keeping a single old one
-# -- add logging
 # Docker:
 # docker build -t colindavey/wp_bak:stub .
 # docker run -v `pwd`/src:/home/mvp/app/src -v `pwd`/dst:/home/mvp/app/dst -t colindavey/wp_bak:stub
 
 from datetime import datetime
-# import logging
+import logging
+import sys
 import tarfile
 import time
 
@@ -23,19 +24,22 @@ WEEK=7*DAY
 
 # Number of seconds between backups
 PERIOD = 2
-# NUM_TO_KEEP = 4
+PERIOD_TO_KEEP = 4
 
 i = 0
 num_reps = 3
 
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+log = logging.getLogger(__name__)
+
 while True:
     time.sleep(PERIOD)
-
-    tar = tarfile.open(DST_DIR+'archive'+datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+'tar.gz', mode='w:gz')
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    tar = tarfile.open(DST_DIR+'archive'+timestamp+'tar.gz', mode='w:gz')
     tar.add(SRC_DIR)
     tar.close
+    log.info('Archiving %s', timestamp)
 
-    print(i)
     i = i + 1
     if(i > num_reps):
         break
